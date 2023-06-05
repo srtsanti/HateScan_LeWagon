73c79f7f93a64eb0083abce310fe7a05b6dd8d6c
Joaquin Ortega <joaquinandresortega84@gmail.com>
2023-06-05 14:48:43 +0200

    adding the twitterapi.py

--- twitterapi.py
+++ twitterapi.py
@@ -1,14 +1,15 @@
 import requests
+from params import *
 
 url = "https://twitter135.p.rapidapi.com/v2/TweetDetail/"
 
-querystring = {"id":"1344794359233998850"}
+querystring = {"id":"{i}"}
 
 headers = {
-	"X-RapidAPI-Key": "17f84b3e3dmsh705aa22617dea22p11e224jsn75fcf7665909",
+	"X-RapidAPI-Key": API_KEY,
 	"X-RapidAPI-Host": "twitter135.p.rapidapi.com"
 }
 
 response = requests.get(url, headers=headers, params=querystring)
 
-print(response.json())
+print(response.json()['data']['threaded_conversation_with_injections_v2']['instructions'][0]['entries'][0]['content']['itemContent']['tweet_results']['result']['legacy']['full_text'])
