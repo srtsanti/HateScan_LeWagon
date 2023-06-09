afde2a0385351c71082a3d2e89fc1141cc439444
Santiago Rodriguez <srtsanti@gmail.com>
2023-06-09 14:19:11 +0200

    Added user_twtitter_api

--- hatescan/api/model_api.py
+++ hatescan/api/model_api.py
@@ -6,6 +6,7 @@ from hatescan.ml_logic.registry import load_model_hatescale, load_model_hatetopi
 from hatescan.ml_logic.preprocessor import preprocessing, X_tokenizer, load_tokenizer_scale_model, load_tokenizer_topic_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import text_to_word_sequence
+from hatescan.api.user_twitter_api import analyse_twitter_profile
 
 
 app = FastAPI()
@@ -60,6 +61,57 @@ def predict(
 
     #Pred Model Topic
     y_pred_topics = model_topic.predict(X_pred_pad_topic)
+    list_topics = list(np.round(y_pred_topics[0], 3))
+    list_topics = [round(float(each), 3) for each in list_topics]
+    dict_topics = dict(zip(range(0,5),list_topics))
+    
+    return {'hate_scale' : pred_scale, 
+            'hate_class': dict_topics}
+
+@app.get("/predict_user")
+def predict(
+        user: str,  # tweet string
+    ):
+
+    #Getting the list of tweets from API Twitter
+    list_of_tweets = analyse_twitter_profile(user)
+    
+    df = pd.DataFrame(list_of_tweets, columns='TweetText')
+    
+    df['Cleaned_text'] = df['TweetText'].apply(preprocessing)
+    
+    X_new = [text_to_word_sequence(_) for _ in df["Cleaned_text"]]
+
+    #Bring Tokenizer X
+    loaded_tokenizer_scale = app.state.tokenizer_scale
+    X_pred_token_scale= loaded_tokenizer_scale.texts_to_sequences(X_new)
+    loaded_tokenizer_topic = app.state.tokenizer_topic
+    X_pred_token_topic= loaded_tokenizer_topic.texts_to_sequences(X_new)
+    
+    #Pad X
+    X_pred_pad_scale = pad_sequences(X_pred_token_scale, dtype='float32', padding='post')
+    X_pred_pad_topic = pad_sequences(X_pred_token_topic, dtype='float32', padding='post')
+        
+    #Bring Modelos
+    model_scale = app.state.model_1
+    model_topic = app.state.model_2
+    
+    #Pred Model Scale
+    result_list = []
+    for ele in X_pred_pad_scale:
+        result_list.append(model_scale.predict(ele))
+    # We need to see what to do as thershold 
+    avg_ypred_scale = np.average(result_list)
+    pred_scale = {'HateLabel': int(np.argmax(avg_ypred_scale))}
+    
+    #Pred Model Topic
+    breakpoint()
+    topic_results = []
+    for ele in X_pred_pad_topic:
+        topic_results.append(model_topic.predict(ele))
+    
+    
+    
     list_topics = list(np.round(y_pred_topics[0], 3))
     list_topics = [round(float(each), 3) for each in list_topics]
     dict_topics = dict(zip(range(0,5),list_topics))
